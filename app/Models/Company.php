b2cb3428a553b3de8b93c7c3f893137364d2954f
trickster1o1 <nischaltuladhar15@gmail.com>
2023-03-04 19:02:20 +0545

    Company / Employee Models and required migration fields created with user seeder

---
+++ app/Models/Company.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    use HasFactory;
+
+    function employee() {
+        return $this->hasMany(Employee::class);
+    }
+}
